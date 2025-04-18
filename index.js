b6bbc79092600b2d69fb0cdaddb39d934815e77a
Dhruv Boghani <dhruvboghani624@gmail.com>
2025-04-18 14:44:15 +0530

    first commit

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.use('/assign', require('./routes/shop/assign'));
 
 
 //connection
-mongoose.connect(MONGO_URI)
+mongoose.connect(MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
