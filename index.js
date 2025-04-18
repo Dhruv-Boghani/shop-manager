22b50db60dbd790634b4bb08be786b6c944926fb
Dhruv Boghani <dhruvboghani624@gmail.com>
2025-04-18 21:23:52 +0530

    tag is work change mongo connection

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.use('/assign', require('./routes/shop/assign'));
 
 
 //connection
-mongoose.connect(MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
