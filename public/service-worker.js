87f13904bd1442c198dd4eb60fc1aa730bcebfb4
Dhruv Boghani <dhruvboghani624@gmail.com>
2025-04-28 16:13:00 +0530

    final touch up 2

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,7 +8,7 @@ self.addEventListener('install', e => {
           '/icons/icon-192.png',
           '/icons/icon-512.png',
           '/icons/favicon.ico',
-          '/icons/logo.png',
+          '/image/logo.png',
           '/sound/beep.pm3',
           '/tags',
           '/qrcodes',
