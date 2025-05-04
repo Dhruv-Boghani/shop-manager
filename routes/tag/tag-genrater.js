186d6a6732a3926904a86f6b5de926b401fee424
Dhruv Boghani <dhruvboghani624@gmail.com>
2025-05-04 18:33:21 +0530

    fix label 6

--- routes/tag/tag-genrater.js
+++ routes/tag/tag-genrater.js
@@ -92,26 +92,23 @@ async function generateTagImage(dataObj, tagDir) {
   const textX = qrX + qrSize + mmToPx(1);
   let textY = qrY + mmToPx(2.5);
 
-  const boldFont = 'bold 60px Impact';        // Or 'Arial Black', 'Roboto Black'
-  const midBoldFont = 'bold 38px Impact';
-  const smallBoldFont = 'bold 32px Impact';
-
   // Price
   ctx.fillStyle = 'black';
-  ctx.font = boldFont;
+  ctx.font = 'bold 60px OpenSans';
   ctx.fillText(`₹ ${dataObj.price}`, textX, textY + 10);
-  ctx.fillText(`₹ ${dataObj.price}`, textX + 1, textY + 10); // extra bold
+  ctx.fillText(`₹ ${dataObj.price}`, textX + 1, textY + 10); // Extra bold effect
 
   // ID (split into 2 lines)
-  ctx.font = midBoldFont;
+  ctx.font = 'bold 38px OpenSans';
   const id = dataObj.id.toString();
   const mid = Math.ceil(id.length / 2);
   const idLine1 = id.slice(0, mid);
   const idLine2 = id.slice(mid);
+
   textY += 64;
   ctx.fillText(idLine1, textX, textY);
-  ctx.fillText(idLine1, textX + 1, textY); // extra bold
-  ctx.fillText(idLine1, textX, textY + 1); // shadow bold
+  ctx.fillText(idLine1, textX + 1, textY); // Extra bold
+  ctx.fillText(idLine1, textX, textY + 1); // Shadow effect
 
   textY += 40;
   ctx.fillText(idLine2, textX, textY);
@@ -119,13 +116,11 @@ async function generateTagImage(dataObj, tagDir) {
   ctx.fillText(idLine2, textX, textY + 1);
 
   // Code (in 1 line)
-  ctx.font = smallBoldFont;
+  ctx.font = 'bold 32px OpenSans';
   textY += 42;
   ctx.fillText(`Code: ${dataObj.code}`, textX, textY);
   ctx.fillText(`Code: ${dataObj.code}`, textX + 1, textY);
   ctx.fillText(`Code: ${dataObj.code}`, textX, textY + 1);
-
-
 
   // Barcode at bottom right
   const maxBarcodeWidth = mmToPx(28);
